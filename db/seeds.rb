da9ca142e64933aecf6fee9a5b068cd558e243c5
snackersteph <swongwrites@gmail.com>
2018-05-07 23:36:40 -0700

    Generate Faker seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Faker::UniqueGenerator.clear
+
+50.times do
+  Customer.create(
+    :firstname => Faker::Name.unique.first_name,
+    :lastname => Faker::Name.unique.last_name,
+    :company_id => Faker::Number.between(1, 10)
+  )
+end
+
+10.times do
+  Company.create(
+    :title => Faker::Company.unique.name
+  )
+end
