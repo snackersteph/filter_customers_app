be9299daca69fc6c36a1bed1dfe8c508c687b883
snackersteph <swongwrites@gmail.com>
2018-05-09 14:36:21 -0700

    Delete old files

--- app/assets/javascripts/components/index.jsx
+++ app/assets/javascripts/components/index.jsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 class App extends React.Component {
   constructor(props) {
     super(props);
--- app/assets/javascripts/components/search.jsx
+++ app/assets/javascripts/components/search.jsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 class Search extends React.Component {
   constructor(props) {
     super(props);
