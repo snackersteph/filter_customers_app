7d5f12b602a7626574d50bb31be944c386b08606
snackersteph <swongwrites@gmail.com>
2018-05-09 14:54:39 -0700

    Add click handler placeholders

--- app/assets/javascripts/components/search.jsx
+++ app/assets/javascripts/components/search.jsx
@@ -5,19 +5,18 @@ class Search extends React.Component {
       current: ''
     }
     this.handleUpdate = this.handleUpdate.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleUpdate(e) {
     this.setState({
       current: e.target.value
     })
-    console.log(this.state.current)
-
-    console.log(this.props.handleChange)
     this.props.handleChange(this.state.current)
-    // .then(() => {
-    //   this.props.getResults(this.state.current)
-    // })
+  }
+
+  handleClick(e) {
+    console.log(e.target.dataset.value)
   }
 
   render() {
@@ -30,7 +29,7 @@ class Search extends React.Component {
                       aria-expanded="false">Company<span className="caret"></span></button>
               <ul className="dropdown-menu">
                 {this.props.companies.map( (company, index) => {
-                  return <li key={index}><a href="#">{company.title}</a></li>
+                  return <li key={index}><a data-value={company.title} onClick={this.handleClick}>{company.title}</a></li>
                 })}
                 <li role="separator" className="divider"></li>
                 <li><a href="#">All Companies</a></li>
@@ -40,12 +39,12 @@ class Search extends React.Component {
               <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" 
                       aria-expanded="false">Sort by<span className="caret"></span></button>
               <ul className="dropdown-menu">
-                <li><a href="#">First Name, Descending</a></li>
-                <li><a href="#">First Name, Ascending</a></li>
-                <li><a href="#">Last Name, Descending</a></li>
-                <li><a href="#">Last Name, Ascending</a></li>
-                <li><a href="#">Company Name, Descending</a></li>
-                <li><a href="#">Company Name, Ascending</a></li>
+                <li ><a data-value="First Name, Descending" onClick={this.handleClick}>First Name, Descending</a></li>
+                <li ><a data-value="First Name, Ascending" onClick={this.handleClick}>First Name, Ascending</a></li>
+                <li><a data-value="Last Name, Descending" onClick={this.handleClick}>Last Name, Descending</a></li>
+                <li><a data-value="Last Name, Ascending" onClick={this.handleClick}>Last Name, Ascending</a></li>
+                <li><a data-value="Company Name, Descending" onClick={this.handleClick}>Company Name, Descending</a></li>
+                <li><a data-value="Company Name, Ascending" onClick={this.handleClick}>Company Name, Ascending</a></li>
               </ul>
             </div>
           </div>
